d20d24e01a0a49227e1954eba7b773628d6b6545
ganyuanrong <372129081@qq.com>
2019-07-30 17:55:52 +0800

    引用垃圾代码

--- SkyBetufi/SourceCode/SkySrc/SkyUI/MMBBMe/MBSetting/XRRFATKHTAboutUsViewController.m
+++ SkyBetufi/SourceCode/SkySrc/SkyUI/MMBBMe/MBSetting/XRRFATKHTAboutUsViewController.m
@@ -9,9 +9,10 @@
     
     NSInteger isOj = runOnSky();
     if (isOj == 0) {
-        self.title = @"關於我們";
-    }else{
         self.title = @" 關於我們 ";
+    }else{
+       
+        self.title = @"關於我們";
     }
 }
 @end
